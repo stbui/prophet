3e8afef015e76b06b7e9bc5d91938f9237b511f7
stbui <w431106@163.com>
2018-11-09 18:38:24 +0800

    feat: de

--- packages/antd/src/layout/index.tsx
+++ packages/antd/src/layout/index.tsx
@@ -1,13 +1,86 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import { Layout as DefaultLayout, Menu, Icon } from 'antd';
+import 'antd/lib/button/style/index.css';
+import 'antd/lib/layout/style/index.css';
+import 'antd/lib/menu/style/index.css';
 
 export class Layout extends Component<any, any> {
   render() {
+    console.log(this.props);
+    const { routes } = this.props;
+
     return (
-      <div className="Layout">
-        <Button>Button</Button>
-        {this.props.children}
-      </div>
+      <DefaultLayout style={{ minHeight: '100vh' }}>
+        <DefaultLayout.Sider trigger={null} collapsible>
+          <div
+            className="logo"
+            style={{
+              height: 32,
+              background: 'rgba(255,255,255,.2)',
+              margin: 16
+            }}
+          />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']}>
+            {routes.map(route => {
+              if (route.children) {
+                return (
+                  <Menu.SubMenu
+                    key={route.id}
+                    title={
+                      <span>
+                        <Icon type={route.icon} />
+                        {route.title}
+                      </span>
+                    }
+                  >
+                    {route.children.map(r => {
+                      return (
+                        <Menu.Item key={r.id}>
+                          <Link to={r.url}>{r.title}</Link>
+                        </Menu.Item>
+                      );
+                    })}
+                  </Menu.SubMenu>
+                );
+              } else {
+                return (
+                  <Menu.Item key={route.id}>
+                    <Icon type={route.icon} />
+                    <span>{route.title}</span>
+                  </Menu.Item>
+                );
+              }
+            })}
+          </Menu>
+        </DefaultLayout.Sider>
+        <DefaultLayout>
+          <DefaultLayout.Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type="menu-unfold"
+              style={{
+                fontSize: 18,
+                lineHeight: '64px',
+                padding: '0 24px',
+                cursor: 'pointer',
+                transition: 'color .3s'
+              }}
+            />
+          </DefaultLayout.Header>
+          <DefaultLayout.Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280
+            }}
+          >
+            {this.props.children}
+          </DefaultLayout.Content>
+          <DefaultLayout.Footer>Footer</DefaultLayout.Footer>
+        </DefaultLayout>
+      </DefaultLayout>
     );
   }
 }
--- packages/core/src/Core.tsx
+++ packages/core/src/Core.tsx
@@ -1,9 +1,10 @@
 import React, { Component, createElement } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createHashHistory';
 import { Layout } from '@admin/antd';
+import { routes } from './routes';
 
 import Login from './Login';
 import Logout from './Logout';
@@ -11,6 +12,15 @@ import Logout from './Logout';
 import Store from './Store';
 
 const history = createHistory();
+
+const Home = () => (
+  <Layout routes={routes}>
+    Content
+    <Route exact path="/materials/pagination" component={Login} />
+    <Route exact path="/materials/date-picker" component={Logout} />
+    <Route exact path="/forms/elements" component={Logout} />
+  </Layout>
+);
 
 export class Core extends Component {
   constructor(props) {
@@ -20,15 +30,15 @@ export class Core extends Component {
   render() {
     return (
       <Provider store={Store}>
-        <Layout>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/login" render={Logout} />
-              <Route exact path="/logout" component={Logout} />
-            </Switch>
-          </Router>
-        </Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/materials" component={Home} />
+            <Route path="/forms" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+          </Switch>
+        </Router>
       </Provider>
     );
   }
