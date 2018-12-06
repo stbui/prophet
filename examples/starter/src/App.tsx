3dc9326522a14d7e804c70b057e779c95ce64f91
stbui <w431106@163.com>
2018-12-06 10:54:01 +0800

    feat: add examples

---
+++ examples/starter/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Admin, Resource } from 'prophet-core';
+import { Layout } from 'prophet-antd';
+import jsonServerProvider from 'prophet-data-json-server';
+
+const List = (props: any) => <div>list</div>;
+const Edit = (props: any) => <div>edit</div>;
+const Create = (props: any) => <div>create</div>;
+const Show = (props: any) => <div>custom</div>;
+const CatchAll = (props: any) => <div>CatchAll</div>;
+
+class App extends Component {
+  render() {
+    return (
+      <Admin
+        dataProvider={jsonServerProvider('http://127.0.0.1')}
+        layout={Layout}
+      >
+        <Resource
+          name="forms"
+          list={List}
+          edit={Edit}
+          create={Create}
+          show={Show}
+        />
+      </Admin>
+    );
+  }
+}
+
+export default App;
