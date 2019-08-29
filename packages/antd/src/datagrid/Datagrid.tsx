27877d2fd8c4e9eab6ff7cd9595eeb5150a953bf
stbui <w431106@163.com>
2019-08-29 18:47:59 +0800

    feat(antd): update

--- packages/antd/src/datagrid/Datagrid.tsx
+++ packages/antd/src/datagrid/Datagrid.tsx
@@ -1,105 +1,83 @@
-import React, { Component, Children } from 'react';
+import React, { Children } from 'react';
 import { Table } from 'antd';
 
 /**
  * <Datagrid>
- *  <Text dataIndex="name">姓名</Text>
- *  <Text dataIndex="age">年龄</Text>
+ *  <Column dataIndex="name">姓名</Column>
+ *  <Column dataIndex="age">年龄</Column>
  * </Datagrid>
  */
 
-export interface IProps {
-    data?: any;
-    ids?: any;
-    children?: any;
-    total?: number;
-    page?: number;
-    perPage?: number;
-    hideOnSinglePage?: any;
-    showQuickJumper?: any;
-    showSizeChanger?: any;
-    setPage?: (page?: any) => void;
-    setPerPage?: (page?: any) => void;
-}
+export const Datagrid = props => {
+    const {
+        children = [],
+        data,
+        ids,
+        page,
+        perPage,
+        total,
+        setPage,
+        setPerPage,
+        showSizeChanger,
+        showQuickJumper,
+        hideOnSinglePage,
+        noData,
+    } = props;
 
-export class Datagrid extends Component<IProps> {
-    static defaultProps = {
-        showSizeChanger: true,
-        showQuickJumper: true,
-        hideOnSinglePage: false,
-        data: [],
-        page: 1,
-    };
+    const handlePageChange = (current, pageSize) => setPage(current);
 
-    handleChange = (
-        pagination: any,
-        filters: any,
-        sorter: any,
-        extra: any
-    ) => {};
+    const handleShowSizeChange = (current, pageSize) => setPerPage(pageSize);
 
-    handlePageChange = (current: number, pageSize: number) => {
-        this.props.setPage(current);
-    };
+    const handleShowTotal = () =>
+        `共 ${total} 条记录 第 ${page}/${Math.ceil(total / perPage)} 页`;
 
-    handleShowSizeChange = (current: number, pageSize: number) => {
-        this.props.setPerPage(pageSize);
-    };
+    const columns = [];
 
-    handleShowTotal = () => {
-        const { page, perPage, total } = this.props;
-        return `共 ${total} 条记录 第 ${page}/${Math.ceil(total / perPage)} 页`;
-    };
-
-    render() {
-        const {
-            children = [],
-            data,
-            ids,
-            page,
-            perPage,
-            total,
-            showSizeChanger,
-            showQuickJumper,
-            hideOnSinglePage,
-        } = this.props;
-        const columns: any = [];
-
-        Children.map(children, (child: any, key: any) => {
-            const { children, dataIndex, ...other } = child.props;
-            columns.push({
-                title: children ? children : dataIndex,
-                dataIndex: dataIndex,
-                key: key,
-                ...other,
-            });
+    Children.map(children, (child, key) => {
+        const { children, dataIndex, ...other } = child.props;
+        columns.push({
+            title: children || dataIndex,
+            dataIndex,
+            key,
+            ...other,
         });
+    });
 
-        const pagination = {
-            showSizeChanger: showSizeChanger,
-            showQuickJumper: showQuickJumper,
-            hideOnSinglePage: hideOnSinglePage,
-            total: total,
-            current: page,
-            defaultPageSize: perPage,
-            showTotal: this.handleShowTotal,
-            onShowSizeChange: this.handleShowSizeChange,
-            onChange: this.handlePageChange,
-        };
+    const pagination = {
+        showSizeChanger,
+        showQuickJumper,
+        hideOnSinglePage,
+        total,
+        current: page,
+        defaultPageSize: perPage,
+        showTotal: handleShowTotal,
+        onShowSizeChange: handleShowSizeChange,
+        onChange: handlePageChange,
+    };
 
-        const newData = ids && ids.map(d => data[d]);
+    const newData = Array.isArray(data) ? data : ids && ids.map(d => data[d]);
 
-        return (
-            <Table
-                rowKey="id"
-                columns={columns}
-                dataSource={newData}
-                onChange={this.handleChange}
-                pagination={pagination}
-                {...this.props}
-            />
-        );
+    if (noData && (!newData || !newData.length)) {
+        return noData;
     }
-}
+
+    return (
+        <Table
+            rowKey="id"
+            columns={columns}
+            dataSource={newData}
+            pagination={pagination}
+            {...props}
+        />
+    );
+};
+
+Datagrid.defaultProps = {
+    showSizeChanger: true,
+    showQuickJumper: true,
+    hideOnSinglePage: false,
+    data: [],
+    page: 1,
+};
 
 export default Datagrid;
