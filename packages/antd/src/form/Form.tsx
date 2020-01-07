a9d15b4229c6e4caa13c379341e43ea85fa9ee39
stbui <w431106@163.com>
2020-01-07 09:52:26 +0800

    feat(antd): wip

--- packages/antd/src/form/Form.tsx
+++ packages/antd/src/form/Form.tsx
@@ -8,24 +8,19 @@ const FormItem = Form.Item;
 const formItemLayout = {
     labelCol: {
         xs: { span: 24 },
-        sm: { span: 8 },
+        sm: { span: 7 },
     },
     wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 16 },
+        sm: { span: 12 },
+        md: { span: 10 },
     },
 };
 
 const tailFormItemLayout = {
     wrapperCol: {
-        xs: {
-            span: 24,
-            offset: 0,
-        },
-        sm: {
-            span: 16,
-            offset: 8,
-        },
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 7 },
     },
 };
 
