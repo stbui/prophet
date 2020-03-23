a269d281ddcabbb04087fcf36b1715f962ef7dbf
stbui <w431106@163.com>
2020-03-23 15:50:11 +0800

    fix(core): 完善useRedirect

--- packages/core/src/sideEffect/useRedirect.ts
+++ packages/core/src/sideEffect/useRedirect.ts
@@ -21,6 +21,17 @@ const resolveRedirectTo = (redirectTo, basePath: string, id?) => {
     }
 };
 
+/**
+ * 重定向
+ * 
+ * @example
+ * 
+ * const redirect = useRedirect();
+ * redirect('list', '/users');
+ * redirect('edit', '/users', 123);
+ * redirect(false);
+ * redirect((redirectTo, basePath, is, data) => ...)
+ */
 const useRedirect = () => {
     const history = useHistory();
     const refresh = useRefresh();
@@ -28,7 +39,16 @@ const useRedirect = () => {
     return useCallback(
         (redirectTo, basePath = '', id) => {
             if (!redirectTo) {
-                refresh();
+                if (history.location.state || history.location.search) {
+                    history.replace({
+                        ...history.location,
+                        state: {},
+                        search: undefined,
+                    });
+                } else {
+                    refresh();
+                }
+
                 return;
             }
 
