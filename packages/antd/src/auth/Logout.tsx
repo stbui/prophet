f4629388587334089db2a70cc5c6daa7876a7920
stbui <w431106@163.com>
2020-11-13 22:36:10 +0800

    fix: rervert react.16.14.0

--- packages/antd/src/auth/Logout.tsx
+++ packages/antd/src/auth/Logout.tsx
@@ -1,3 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useLogout, useTranslate } from '@stbui/prophet-core';
+import { Menu } from 'antd';
 
-export default () => <div>Logout</div>;
+export default ({ redirectTo }) => {
+    const logout = useLogout();
+    const translate = useTranslate();
+
+    const onClick = useCallback(() => logout(null, redirectTo, false), [
+        redirectTo,
+        logout,
+    ]);
+
+    return (
+        <Menu.Item onClick={onClick}>
+            {translate('prophet.auth.logout')}
+        </Menu.Item>
+    );
+};
